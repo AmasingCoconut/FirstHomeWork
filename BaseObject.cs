4c99267eb20bedb65ef0ce0ad7880ecd9a01d12f
AmasingCoconut <58276535+AmasingCoconut@users.noreply.github.com>
2019-11-28 02:28:43 +0300

    Add files via upload

---
+++ BaseObject.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Drawing;
+
+namespace MyGame
+{
+    class BaseObject
+    {
+        protected Point Pos;
+        protected Point Dir;
+        protected Size Size;
+        public BaseObject(Point pos, Point dir, Size size)
+        {
+            Pos = pos;
+            Dir = dir;
+            Size = size;
+        }
+        public virtual void Draw()
+        {
+            Game.Buffer.Graphics.DrawEllipse(Pens.Blue, Pos.X, Pos.Y, Size.Width, Size.Height);
+        }
+        public virtual void Update()
+        {
+            Pos.X = Pos.X + Dir.X;
+            Pos.Y = Pos.Y + Dir.Y;
+            if (Pos.X < 0) Pos.X = Game.Width + Size.Width;
+            if (Pos.Y < 0) Pos.Y = Game.Height + Size.Height;
+            //if (Pos.X < 0) Dir.X = -Dir.X;
+            //if (Pos.X > Game.Width) Dir.X = -Dir.X;
+            //if (Pos.Y < 0) Dir.Y = -Dir.Y;
+            //if (Pos.Y > Game.Height) Dir.Y = -Dir.Y;
+        }
+    }
+}
